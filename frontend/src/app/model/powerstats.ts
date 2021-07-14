1225caeae429276044146d5ac21c28cd76603330
Evelin <ladyblue@freemail.hu>
2021-07-14 18:30:28 +0200

    Model / Superhero

---
+++ frontend/src/app/model/powerstats.ts
@@ -0,0 +1,8 @@
+export class Powerstats {
+  intelligence: number = 0;
+  strength: number = 0;
+  speed: number = 0;
+  durability: number = 0;
+  power: number = 0;
+  combat: number = 0;
+}
---
+++ frontend/src/app/model/superhero.ts
@@ -0,0 +1,18 @@
+import { Appearance } from "./appearance";
+import { Biography } from "./biography";
+import { Connections } from "./connections";
+import { Images } from "./images";
+import { Powerstats } from "./powerstats";
+import { Work } from "./work";
+
+export class Superhero {
+    _id: string = '';
+    name: string = '';
+    slug: string = '';
+    powerstats: Powerstats = new Powerstats();
+    appearance: Appearance = new Appearance();
+    biography: Biography = new Biography();
+    work: Work = new Work();
+    connections: Connections = new Connections();
+    images: Images = new Images();
+}
