df55e16e30a53bfd402c3bf70a760a857dce0d99
Evelin <ladyblue@freemail.hu>
2021-07-26 18:49:14 +0200

    List / Superheroes (pagination + complex filter)

--- frontend/src/app/page/superheroes/superheroes.component.ts
+++ frontend/src/app/page/superheroes/superheroes.component.ts
@@ -3,6 +3,11 @@ import { Observable } from 'rxjs';
 import { Superhero } from 'src/app/model/superhero';
 import { SuperheroService } from 'src/app/service/superhero.service';
 import { ITableColumn, ConfigService } from 'src/app/service/config.service';
+import { tap } from 'rxjs/operators';
+
+interface IPageBtn {
+  page: number;
+}
 
 @Component({
   selector: 'app-superheroes',
@@ -14,7 +19,9 @@ export class SuperheroesComponent implements OnInit {
   copyright: string = 'Data provided by SuperHero API';
 
   tableColumns: ITableColumn[] = this.config.superheroColumns;
-  list$: Observable<Superhero[]> = this.superheroService.getAll();
+  list$: Observable<Superhero[]> = this.superheroService.getAll().pipe(
+    tap( superheroes => this.superheroesProperties.count = superheroes.length )
+  );
 
   constructor(
     private config: ConfigService,
@@ -22,6 +29,51 @@ export class SuperheroesComponent implements OnInit {
     ) { }
 
   ngOnInit(): void {
+  }
+
+  onDelete(_id: string): void {
+    this.superheroService.delete(_id);
+  }
+
+  superheroesProperties: {count: number} = {
+    count: 0,
+  };
+  pageSize: number = 10;
+  pageStart: number = 1;
+  currentPage: number = 1;
+  get paginator(): IPageBtn[] {
+    const pages: IPageBtn[] = [];
+    for (let i = 0; i < this.superheroesProperties.count / this.pageSize && pages.length < 10; i++ ) {
+      const page = this.pageStart + i;
+      pages.push({page});
+    }
+    return pages;
+  }
+  get pageSliceStart(): number {
+    const index = this.currentPage - 1;
+    return index === 0 ? 0 : (index * this.pageSize);
+  }
+  get pageSliceEnd(): number {
+    return this.pageSliceStart + this.pageSize;
+  }
+
+  onPaginate(event: Event, btn: IPageBtn): void {
+    event.preventDefault();
+    this.currentPage = btn.page;
+    this.pageStart = (btn.page - 5) < 1 ? 1 : (btn.page - 5);
+  }
+
+  onStepPage(event: Event, step: number): void {
+    event.preventDefault();
+    this.currentPage += step;
+    this.pageStart = (this.currentPage - 5) < 1 ? 1 : (this.currentPage - 5);
+  }
+
+  filterKey: string = '';
+  phrase: string = '';
+
+  searchEvent(event: Event): void {
+    this.phrase = (event.target as HTMLInputElement).value;
   }
 
 }
