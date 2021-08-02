e67ee119fc2e697d9517d600456106a73c797380
Evelin <ladyblue@freemail.hu>
2021-08-02 18:37:45 +0200

    Editor / Superhero (demo version)

--- frontend/src/app/page/superhero-editor/superhero-editor.component.ts
+++ frontend/src/app/page/superhero-editor/superhero-editor.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { Superhero } from 'src/app/model/superhero';
+import { ConfigService, ITableColumn } from 'src/app/service/config.service';
+import { SuperheroService } from 'src/app/service/superhero.service';
 
 @Component({
   selector: 'app-superhero-editor',
@@ -7,9 +13,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuperheroEditorComponent implements OnInit {
 
-  constructor() { }
+  fields: ITableColumn[] = this.configService.superheroColumns;
+  updating : boolean = false;
+  superhero$: Observable<Superhero | undefined> = of(new Superhero() );
+
+  superhero: Superhero = new Superhero();
+
+  constructor(
+    private superheroService: SuperheroService,
+    private activatedRoute: ActivatedRoute,
+    private configService: ConfigService,
+    private router: Router,
+    // private notifyService : NotificationService,
+  ) { }
 
   ngOnInit(): void {
+    this.superheroService.getAll();
+    this.activatedRoute.params.subscribe(
+      params => {
+        console.log(params.id);
+        if (params.id == 0) {
+          this.superhero$ = of( new Superhero() );
+        } else {
+          this.superhero$ = this.superheroService.get(params.id);
+        }
+      })
   }
 
+  /*
+  showHtmlToasterUpdate(){
+    this.notifyService.showHTMLMessage(`Updating was successful.`, ``, 3000)
+  }
+  */
+
+  onUpdate(form: NgForm, superhero: Superhero): void {
+
+    this.updating = true;
+
+    this.superheroService.update(superhero).subscribe(
+      () => this.router.navigate(['superheroes'])
+    )
+}
 }
