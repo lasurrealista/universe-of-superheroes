79b37d9bc40b091f126c546ff24fe02fb99e1439
Evelin <ladyblue@freemail.hu>
2021-08-05 17:01:01 +0200

    Areus-form

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { AreusFormModule } from './areus-form/areus-form.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -67,6 +68,7 @@ import { UserEditorComponent } from './page/user-editor/user-editor.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    AreusFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
