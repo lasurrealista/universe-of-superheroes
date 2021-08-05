e9b8211be8e7cd9c6bfb8a8f0e33f90ca71a891e
Evelin <ladyblue@freemail.hu>
2021-08-05 18:08:21 +0200

    Edit / Marvel Character + validation

--- frontend/src/app/page/marvel-character-editor/marvel-character-editor.component.ts
+++ frontend/src/app/page/marvel-character-editor/marvel-character-editor.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
+import { FieldBase } from 'src/app/areus-form/model/field-base';
+import { InputField } from 'src/app/areus-form/model/input-field';
+import { TextareaField } from 'src/app/areus-form/model/textarea-field';
 import { MarvelCharacter } from 'src/app/model/marvel-character';
 import { MarvelCharacterService } from 'src/app/service/marvel-character.service';
 
@@ -17,6 +21,9 @@ export class MarvelCharacterEditorComponent implements OnInit {
   );
   marvelCharacter: MarvelCharacter = new MarvelCharacter();
 
+  fields: FieldBase<any>[] = [];
+  showForm: boolean = false;
+
   constructor(
     private marvelCharacterService: MarvelCharacterService,
     private ar: ActivatedRoute,
@@ -24,6 +31,42 @@ export class MarvelCharacterEditorComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.marvelCharacter$.subscribe(
+      marvelCharacter => {
+        this.marvelCharacter = marvelCharacter;
+        this.setForm();
+        this.showForm = true;
+      }
+    );
+  }
+
+  setForm(): void {
+    /*
+    _id : number | string = '';
+    name : string = '';
+    thumbnail : string = '';
+    profile : string = '';
+    url : string = '';
+    stories : number = 0
+  */
+    this.fields = [
+      new InputField({key: '_id', label: '', type: 'hidden', value: this.marvelCharacter._id}),
+      new InputField({key: 'name', label: 'Name', type: 'text', value: this.marvelCharacter.name,
+        validators: [Validators.required, Validators.pattern(/^[A-Za-z0-9][ A-Za-z0-9_@./#&+-]{0,24}/)],
+        errorMessage: 'The name of the character must be between 1 and 25 characters.'}),
+      new InputField({key: 'thumbnail', label: 'Image', type: 'text', value: this.marvelCharacter.thumbnail,
+        validators: [Validators.required, Validators.pattern(/(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+(?:png|jpg|jpeg|gif)+$/)],
+        errorMessage: 'The image of the character must be a valid URL ending with .jpg, .png, .gif and .jpeg.'}),
+      new InputField({key: 'profile', label: 'Profile', type: 'text', value: this.marvelCharacter.profile,
+        validators: [Validators.required, Validators.pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/)],
+        errorMessage: 'The profile of the character must be a valid URL.'}),
+      new InputField({key: 'url', label: 'Homepage', type: 'text', value: this.marvelCharacter.url,
+        validators: [Validators.required, Validators.pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/)],
+        errorMessage: 'The homepage of the character must be a valid URL.'}),
+      new InputField({key: 'stories', label: 'Stories', type: 'number', value: this.marvelCharacter.stories,
+        validators: [Validators.required, Validators.pattern(/^[0-9]{2}$/)],
+        errorMessage: 'This should be a two-digit number.'}),
+    ];
   }
 
   onSave(marvelCharacter: MarvelCharacter): void {
